3d68aa50c7fff626631ad5ed0025c1248dba0d52
Tiago <ottiagos@gmail.com>
2022-07-19 00:45:48 -0300

    basic view with right statusbar spacing

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-web';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+
+const isAndroid = Platform.OS === "android";
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>Hello world</Text>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View
+          style={{
+            padding: 16,
+            backgroundColor: "green",
+            marginTop: isAndroid ? StatusBar.currentHeight : 0,
+          }}
+        >
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: "blue" }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container : {
+  container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
